1e0ee1bebf5c4ef19c75cc384c29111ea65efb2f
F0oocuS <ihoryaremko@gmail.com>
2022-10-06 16:51:07 +0300

    create map-canvas component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { MapComponent } from './components/map/map.component';
 import { MapTileComponent } from './components/map/map-tile/map-tile.component';
 import { MapCellComponent } from './components/map/map-cell/map-cell.component';
+import { MapCanvasComponent } from './components/map-canvas/map-canvas.component';
 
 @NgModule({
 	declarations: [
 		AppComponent,
 		MapComponent,
 		MapTileComponent,
-		MapCellComponent
+		MapCellComponent,
+		MapCanvasComponent
 	],
 	imports: [
 		BrowserModule,
